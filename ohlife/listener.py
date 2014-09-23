73d6b3a636fa3a913470ad6011a8da78011e14fa
Sean Harnett <srharnett@gmail.com>
2014-09-22 20:58:12 -0400

    listener actually does something real

--- ohlife/listener.py
+++ ohlife/listener.py
@@ -1,3 +1,5 @@
+import re
+import sqlite3
 from flask import request, Flask, render_template_string
 
 app = Flask(__name__)
@@ -5,14 +7,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print('home')
     return render_template_string('home')
 
 
 @app.route('/listen', methods=['POST'])
 def listen():
-    print('listen', request.form)
+    subject = request.form.get('headers[Subject]')
+    entry = request.form.get('plain')
+
+    def extract_day(s):
+        pattern = r'(\d\d\d\d-\d\d-\d\d)'
+        m = re.search(pattern, s)
+        if m is not None:
+            return m.group(1)
+    day = extract_day(subject)
+
+    print(day)
+    print(entry)
+
+    write_to_db(day, entry)
+
     return render_template_string('listen')
 
+
+def write_to_db(day, entry):
+    if day is not None and entry is not None:
+        try:
+            with sqlite3.connect('db.db') as db:
+                query = 'insert into entries (day, entry) values (?, ?)'
+                db.execute(query, (day, entry))
+                db.commit()
+            print('successfully wrote to db')
+        except:
+            print('ERROR: failed to write to db')
+            raise
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=80, debug=True)
+    app.run('0.0.0.0', port=8000)
