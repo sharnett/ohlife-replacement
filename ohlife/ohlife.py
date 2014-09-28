bc922d04754ab6f914083e8a44e157c9833256ac
Jason Ting <jzting@gmail.com>
2014-09-27 19:44:52 -0700

    A few improvements:
    - Add "reply-to" header so you can just reply instead of forward
    - Add logic to pick an entry by year/month/week/random
    - Updated table so that day column isn't unique so you can have multiple entries per day
    - Add importer
    - Update README

--- ohlife/ohlife.py
+++ ohlife/ohlife.py
@@ -5,35 +5,55 @@ from email.mime.text import MIMEText
 from email.header import Header
 from email_credentials import email_credentials
 
-
-def send_mail(data):
+def send_mail(time_ago, data):
     today = arrow.now().format('dddd, MMM D (YYYY-MM-DD)')
     subject = "It's %s - How did your day go?" % today
-    body = "Forward this email to the secret address with your entry.\n\n" \
-           "Remember this? One year ago you wrote...\n" + data
-    mailhost, fromaddr, toaddrs, credentials = email_credentials()
+    body = "Just reply to this email with your entry.\n\n" \
+           "Remember this? %s ago you wrote...\n\n%s" % (time_ago, data)
+    mailhost, fromaddr, replytoaddr, toaddrs, credentials = email_credentials()
     username, password = credentials
     msg = MIMEText(body, _charset="UTF-8")
     msg['Subject'] = Header(subject, "utf-8")
+    msg['reply-to'] = replytoaddr
     server = smtplib.SMTP(mailhost)
     server.starttls()
     server.login(username, password)
     server.sendmail(fromaddr, toaddrs, msg.as_string())
     server.quit()
 
+def get_entry():
+  last_year = arrow.now().replace(years=-1).format('YYYY-MM-DD')
+  last_month = arrow.now().replace(months=-1).format('YYYY-MM-DD')
+  last_week = arrow.now().replace(weeks=-1).format('YYYY-MM-DD')
 
-def get_last_year_entry():
-    last_year = arrow.now().replace(years=-1).format('YYYY-MM-DD')
-    with sqlite3.connect('db.db') as db:
-        query = 'select entry from entries where day = ?'
-        result = db.execute(query, (last_year,)).fetchone()
-        return result[0] if result is not None else ''
+  day_query = 'select entry from entries where day = ? order by random()'
+  random_query = 'select day, entry from entries order by random()'
 
+  with sqlite3.connect('ohlife.db') as db:
+    result = db.execute(day_query, (last_year,)).fetchone()
+    if result:
+      return ("One year", result[0])
+
+    result = db.execute(day_query, (last_month,)).fetchone()
+    if result:
+      return ("One month", result[0])
+
+    result = db.execute(day_query, (last_week,)).fetchone()
+    if result:
+      return ("One week", result[0])
+
+    result = db.execute(random_query).fetchone()
+    if result:
+      num_days_ago = (arrow.now() - arrow.get(result[0])).days - 1
+      return ("%d days" % num_days_ago, result[1])
+
+    return (None, None)
 
 def main():
-    data = get_last_year_entry()
-    send_mail(data)
+    data, time_ago = get_entry()
 
+    if data and time_ago:
+      send_mail(data, time_ago)
 
 if __name__ == '__main__':
     main()
